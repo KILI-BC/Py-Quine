233684dc7dd838ca097c2d143946bff1b80ab5f4
KILI-BC <45309327+KILI-BC@users.noreply.github.com>
2026-01-01 23:52:05 +0100

    Commited current progress

---
+++ src/quine.py
@@ -0,0 +1,26 @@
+# ASSETS:
+newline = "\n"
+backslash = "\\"
+s = """f.writelines("# ASSETS:" + newline)
+f.writelines('newline = "' + backslash + 'n"' + newline)
+f.writelines('backslash = "' + backslash + backslash + '"' + newline)
+f.writelines("s = " + '"' * 3)
+f.writelines(s)
+f.writelines('"' * 3 + newline)
+f.writelines("# CODE:" + newline)
+f.writelines('with open("quine_cpy.py", "wt") as f:' + newline)
+for l in s.splitlines():
+    f.writelines("    " + l + newline)
+"""
+# CODE:
+with open("quine.py", "wt") as f:
+    f.writelines("# ASSETS:" + newline)
+    f.writelines('newline = "' + backslash + 'n"' + newline)
+    f.writelines('backslash = "' + backslash + backslash + '"' + newline)
+    f.writelines("s = " + '"' * 3)
+    f.writelines(s)
+    f.writelines('"' * 3 + newline)
+    f.writelines("# CODE:" + newline)
+    f.writelines('with open("quine.py", "wt") as f:' + newline)
+    for l in s.splitlines():
+        f.writelines("    " + l + newline)
